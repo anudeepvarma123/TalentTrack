88dda5e4be526d4564139528ca9963399e480600
anudeepvarma123 <anudeepvarma1157@gmail.com>
2025-07-18 12:11:11 +0530

    allowed all ports

--- app/main.py
+++ app/main.py
@@ -28,6 +28,9 @@ app.include_router(leave.router, prefix="/leaves", tags=["Leaves"])
 def home():
     return {"message": "Welcome to TalentTrack HRM System"}
 
+import os
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
